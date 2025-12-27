246d1676c73da1fb07a144e2d170334cac60ae22
Your Name <you@example.com>
2025-12-27 13:19:59 +0700

    optimize toolvoice

--- tool_voices/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Tool Voice Cloning & Synthesis - Voice cloning and TTS with emotional control."""
-
-__version__ = "1.0.0"
--- tool_voices/domain/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Domain models for the voice tool."""
-
-from .emotion import EmotionProfile, default_emotions
-from .voice_profile import VoiceProfile
-
-__all__ = ["EmotionProfile", "default_emotions", "VoiceProfile"]
-
